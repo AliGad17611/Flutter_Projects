014900efcd0417bbcbc40b3648c025f8b0386c2f
Ali gad <aligad.17611@gmail.com>
2024-03-04 20:23:46 +0200

    this is task3 (stack & Positoned widgets)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -26,149 +26,113 @@ class FacebookApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          shadowColor: Colors.black,
-          backgroundColor: Colors.white,
-          elevation: 10,
-          title: Text(
-            "Facebook",
-            style: TextStyle(
-                fontSize: 30,
-                color: Colors.blueAccent,
-                fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-          leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
+      appBar: AppBar(
+        shadowColor: Colors.black,
+        backgroundColor: Colors.white,
+        elevation: 10,
+        title: Text(
+          "Facebook",
+          style: TextStyle(
+              fontSize: 30,
               color: Colors.blueAccent,
-              Icons.menu,
-              size: 30,
-            ),
-          ),
-          actions: [
-            IconButton(
-                color: Colors.blueAccent,
-                onPressed: () {},
-                icon: Icon(
-                  Icons.message,
-                  size: 25,
-                )),
-            IconButton(
-                color: Colors.blueAccent,
-                onPressed: () {},
-                icon: Icon(
-                  Icons.search,
-                  size: 28,
-                ))
-          ],
+              fontWeight: FontWeight.bold),
         ),
-        body: SingleChildScrollView(
-            child: Column(
-          children: [
-            Container(
-              height: 400,
-              width: 420,
-              margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.circular(30)),
-              child: Text(
-                "Flutter Task 2",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            color: Colors.blueAccent,
+            Icons.menu,
+            size: 30,
+          ),
+        ),
+        actions: [
+          IconButton(
+              color: Colors.blueAccent,
+              onPressed: () {},
+              icon: Icon(
+                Icons.message,
+                size: 25,
+              )),
+          IconButton(
+              color: Colors.blueAccent,
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                size: 28,
+              ))
+        ],
+      ),
+      body: Center(
+        child: Container(
+          width: 350,
+          height: 350,
+          padding: EdgeInsets.all(10),
+          color: Colors.blueGrey,
+          child: Stack(children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Container(
+                color: Colors.red[300],
+                height: 120,
+                width: 120,
+                alignment: Alignment.center,
+                child: Text("Ali",
+                    style: TextStyle(fontSize: 20, color: Colors.white)),
               ),
             ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(children: [
-                Container(
-                  height: 150,
-                  width: 200,
-                  margin: EdgeInsets.fromLTRB(0, 20, 20, 20),
-                  alignment: Alignment
-                      .center, //EdgeInsets.fromLTRB(left, top, right, bottom),
-                  decoration: BoxDecoration(
-                      color: Colors.blueGrey,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(
-                    "Ali",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  height: 150,
-                  width: 200,
-                  margin: EdgeInsets.all(20),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      color: Colors.blueGrey,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(
-                    "Gad",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  height: 150,
-                  width: 200,
-                  margin: EdgeInsets.all(20),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      color: Colors.blueGrey,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(
-                    "Ali",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  height: 150,
-                  width: 200,
-                  margin: EdgeInsets.fromLTRB(20, 20, 0, 20),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      color: Colors.blueGrey,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(
-                    "Gad",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ]),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: Container(
+                color: const Color.fromARGB(255, 68, 65, 38),
+                height: 120,
+                width: 120,
+                alignment: Alignment.center,
+                child: Text("Gad",
+                    style: TextStyle(fontSize: 20, color: Colors.white)),
+              ),
             ),
-            Container(
-              height: 400,
-              width: 420,
-              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.circular(30)),
-              child: Text(
-                "1/3/2024",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: Container(
+                color: Colors.green[300],
+                height: 120,
+                width: 120,
+                alignment: Alignment.center,
+                child: Text("Ali",
+                    style: TextStyle(fontSize: 20, color: Colors.white)),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                color: Color.fromARGB(255, 229, 115, 185),
+                height: 120,
+                width: 120,
+                alignment: Alignment.center,
+                child: Text("Gad",
+                    style: TextStyle(fontSize: 20, color: Colors.white)),
+              ),
+            ),
+            Center(
+              child: Positioned(
+                child: Container(
+                  color: const Color.fromARGB(255, 115, 159, 229),
+                  height: 140,
+                  width: 140,
+                  alignment: Alignment.center,
+                  child: Text("Eng",
+                      style: TextStyle(fontSize: 20, color: Colors.white)),
+                ),
               ),
             )
-          ],
-        )));
+          ]),
+        ),
+      ),
+    );
   }
 }
